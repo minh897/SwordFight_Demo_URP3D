5d6bbde8a05b5707c0ad682c3794c3a355ca7584
minh897 <minhnhn897@gmail.com>
2025-10-30 11:03:17 +0700

    Add PlayerCombat and enemy dummy
    - Implement PlayerCombat to initiate attack based-on attack input value read from PlayerInputHandler and cooldown (no real attacking logic yet)
    - Add Player and Enemy prefab based-on a shared model
    - Also refactor some of the variable name

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,16 +1,51 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject weapon;
+    [SerializeField] private float damage = 5.0f;
+    [SerializeField] private float attackCooldown = 0.5f;
+
+    private PlayerInputHandler _inputHandler;
+    
+    // attack timers
+    private float attackTimer;
+    private float nextTimeAttack;
+
     void Start()
     {
-        
+        _inputHandler = GetComponent<PlayerInputHandler>();
+
+        weapon.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        attackTimer = Time.time;
+
+        var inputValue = _inputHandler.AttackInput;
+
+        if (!inputValue)
+            return;
+
+        if (attackTimer > nextTimeAttack)
+        {
+            // Do attack logic and play animation
+            weapon.SetActive(true);
+            Debug.Log("Player attacks!");
+
+            // set cooldown for the next attack
+            nextTimeAttack = attackCooldown + Time.time;
+        }
+        else
+        {
+            weapon.SetActive(false);
+        }
+    }
+
+    private IEnumerator AttackAnimRoutine()
+    {
+        yield return null;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,38 +6,39 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private float rotateSpeed = 5.0f;
 
-    private PlayerInputHandler m_inputHandler;
-    private Rigidbody m_rb;
+    private PlayerInputHandler _inputHandler;
+    private Rigidbody _rb;
 
-    private Vector3 moveDir;
-    private Vector3 currentPos;
-    private Vector3 lastPos;
+    private Vector3 _moveDir;
+    private Vector3 _currentPos;
+    private Vector3 _lastPos;
     
     void Start()
     {
-        m_inputHandler = GetComponent<PlayerInputHandler>();
-        m_rb = GetComponent<Rigidbody>();
+        _inputHandler = GetComponent<PlayerInputHandler>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
         // read input from PlayerInputHandler
-        moveDir = new(m_inputHandler.MoveInput.x, 0, m_inputHandler.MoveInput.y);
+        _moveDir = new(_inputHandler.MoveInput.x, 0, _inputHandler.MoveInput.y);
         
-        // rotate the player facing moving direction smoothly
-        // stop the rotation when the direction magnitude get smaller
-        currentPos = moveDir;
-        if (moveDir.sqrMagnitude > 0.0001f) // this allow more control of the rotation
+        // record the player last position only there are movements
+        // allow rotation to continue even when the moving input is zero 
+        _currentPos = _moveDir;
+        if (_moveDir.sqrMagnitude > 0.0001f) // this allow more control of the rotation
         {
-            lastPos = currentPos;
+            _lastPos = _currentPos;
         } 
-        Quaternion targetRotation = Quaternion.LookRotation(lastPos);
-        Quaternion newRotation = Quaternion.Lerp(m_rb.rotation, targetRotation, Time.fixedDeltaTime * rotateSpeed);
-        m_rb.MoveRotation(newRotation);
+        Quaternion targetRotation = Quaternion.LookRotation(_lastPos);
+        // rotate the player facing moving direction smoothly
+        Quaternion newRotation = Quaternion.Lerp(_rb.rotation, targetRotation, Time.fixedDeltaTime * rotateSpeed);
+        _rb.MoveRotation(newRotation);
 
         // move the player rigidbody via Rigidbody.MovePosition()
-        Vector3 targetPosition = m_rb.position + moveSpeed * Time.fixedDeltaTime * moveDir;
-        m_rb.MovePosition(targetPosition);
+        Vector3 targetPosition = _rb.position + moveSpeed * Time.fixedDeltaTime * _moveDir;
+        _rb.MovePosition(targetPosition);
     }
 
     void OnDrawGizmos()
@@ -51,23 +52,5 @@ public class PlayerMovement : MonoBehaviour
 
         Handles.color = Color.red;
         Handles.ArrowHandleCap(0, position, rotation, arrowLength, EventType.Repaint);
-    }
-
-    // Smoothly rotate the enemy game object to face the given target position
-    private void FaceTarget(Vector3 targetPos)
-    {
-        // Calculate the direction from current position to next target
-        Vector3 directionToTarget = targetPos - transform.position;
-
-        // Ignore any difference in vertical position
-        directionToTarget.y = 0;
-
-        // Create a new rotation that points the forward vector (y) of the game object up to the calculated direction
-        // Since we ignore the vertical position above, the game object can only rotate horizontally
-        Quaternion newRot = Quaternion.LookRotation(directionToTarget);
-
-        // Create a smooth rotation from the current rotation to the target rotation at a defined speed
-        // Time.deltaTime makes this framerate independent
-        transform.rotation = Quaternion.Lerp(transform.rotation, newRot, rotateSpeed * Time.deltaTime);
     }
 }
