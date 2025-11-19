6fec6a9127325fe329b419a9f191467d74fbed2c
minh897 <minhnhn897@gmail.com>
2025-11-19 15:10:21 +0700

    Add enemy pivoting when get hit
    Enemy pivot backward when get hit by weapon. Part of the enemy basic feedback (unfinished)

--- Assets/Scripts/EnemyHitDetection.cs
+++ Assets/Scripts/EnemyHitDetection.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 [RequireComponent(typeof(Health))]
 public class EnemyHitDetection : MonoBehaviour
 {
-    [SerializeField] private Transform pivotPoint;
-    [SerializeField] private float stunDuration = 3f;
-    [SerializeField] private float pushBackDistance = 1f;
+    [SerializeField] private float stunDuration;
+    [SerializeField] private float pushDuration;
+    [SerializeField] private float pushBackDistance;
+    [SerializeField] private float pivotDuration;
+    [SerializeField] private Vector3 pivotAngle;
 
     private Health myWellBeing;
 
     private bool isStunned = false;
-    private int weaponDamage;
     private Coroutine getHitRoutine;
+
+    private float currentAngleX;
+    private float targetAngleX;
 
     void Start()
     {
         myWellBeing = GetComponent<Health>();
+
+        currentAngleX = transform.localRotation.x;
+        targetAngleX = currentAngleX + pivotAngle.x;
     }
 
     void OnTriggerEnter(Collider other)
@@ -44,25 +51,26 @@ public class EnemyHitDetection : MonoBehaviour
         int damage = weapon.GetComponentInParent<PlayerCombat>().GetWeaponDamage();
         // receive damage through IDamageable
         myWellBeing.TakeDamage(damage);
-        // get enemy direction
-        Vector3 direction = gameObject.GetComponent<EnemyFollow>().GetMovingDirection();
 
-        yield return null;
+        // var pushRoutine = StartCoroutine(GetPushBackRoutine(pushDuration, direction));
+        yield return StartCoroutine(GetPivotRoutine(pivotDuration));
 
         isStunned = false;
     }
 
-    private IEnumerator GetPushBackRoutine(float duration, Vector3 direction)
+    private IEnumerator GetPushBackRoutine(float duration)
     {
         float elapsed = 0f;
+        // get enemy direction
+        Vector3 direction = gameObject.GetComponent<EnemyFollow>().GetMovingDirection();
         // flip the current direction to get the opposite direction
         Vector3 oppositeDir = -1 * direction;
         // get enemy current position
         Vector3 startPos = transform.position;
         // calculate the target position
         Vector3 endPos = startPos + oppositeDir * pushBackDistance;
+
         // interpolate between current position and target position
-        
         while (elapsed < duration)
         {
             elapsed += Time.deltaTime;
@@ -73,5 +81,22 @@ public class EnemyHitDetection : MonoBehaviour
         }
 
         transform.position = endPos;
+    }
+
+    private IEnumerator GetPivotRoutine(float duration)
+    {
+        float elapsed = 0f;
+        float startAngleX = currentAngleX;
+        float endAngleX = targetAngleX;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+            float newAngleX = Mathf.LerpAngle(startAngleX, endAngleX, t);
+            transform.rotation = Quaternion.Euler(newAngleX, transform.eulerAngles.y, 0);
+            yield return null;
+        }
+
     }
 }
